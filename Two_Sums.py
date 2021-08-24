b64be011abbbd9daaf133dca4bf7be5d941ed615
konstter <konst.br@gmail.com>
2021-08-24 23:42:20 +0800

    Two Sums v3

--- Two_Sums.py
+++ Two_Sums.py
@@ -6,17 +6,16 @@ def twoSum(nums, target):
     :param target: int
     :return: List[int]
     '''
-    temp_dict = {}
-    for i in range(len(nums)):
-        temp_dict[nums[i]] = i
+    cache_list = nums.copy()
     nums.sort()
     i = 0
     j = -1
     output_list = []
     while True:
         if nums[i] + nums[j] == target:
-            output_list.append(temp_dict.get(nums[i]))
-            output_list.append(temp_dict.get(nums[j]))
+            output_list.append(cache_list.index(nums[i]))
+            cache_list[cache_list.index(nums[i])] = 'used'
+            output_list.append(cache_list.index(nums[j]))
             return output_list
         elif nums[i] + nums[j] < target:
             i += 1
@@ -24,8 +23,11 @@ def twoSum(nums, target):
             j -= 1
 
 
-n = [1, 2, 10, 7, 8]
-t = 15
+n = [3, 3]
+t = 6
+
+# n = [1, 2, 10, 7, 8]
+# t = 15
 
 print(twoSum(n, t))
 
