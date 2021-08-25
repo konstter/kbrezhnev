1dbc4906cca3adbadf8596d0de5515e347d26cc1
konstter <konst.br@gmail.com>
2021-08-25 08:40:13 +0800

    Two Sums v4

--- Two_Sums.py
+++ Two_Sums.py
@@ -6,28 +6,28 @@ def twoSum(nums, target):
     :param target: int
     :return: List[int]
     '''
-    cache_list = nums.copy()
-    nums.sort()
+    index_list = [k for k in range(len(nums))]
+    num = zip(nums, index_list)
+    sorted_list = sorted(num, key=lambda tup: tup[0])
     i = 0
     j = -1
     output_list = []
     while True:
-        if nums[i] + nums[j] == target:
-            output_list.append(cache_list.index(nums[i]))
-            cache_list[cache_list.index(nums[i])] = 'used'
-            output_list.append(cache_list.index(nums[j]))
+        if sorted_list[i][0] + sorted_list[j][0] == target:
+            output_list.append(sorted_list[i][1])
+            output_list.append(sorted_list[j][1])
             return output_list
-        elif nums[i] + nums[j] < target:
+        elif sorted_list[i][0] + sorted_list[j][0] < target:
             i += 1
         else:
             j -= 1
 
 
-n = [3, 3]
-t = 6
+# n = [3, 3]
+# t = 6
 
-# n = [1, 2, 10, 7, 8]
-# t = 15
+n = [1, 2, 10, 7, 8]
+t = 15
 
 print(twoSum(n, t))
 
