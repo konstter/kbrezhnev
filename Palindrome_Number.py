3db730b709be608c5c1d32e6ef1d3588535f1ebd
konstter <konst.br@gmail.com>
2021-08-29 20:52:16 +0800

    9. Palindrome Number v3

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -6,28 +6,34 @@ def ispalindrome(x):
     :param x: Integer
     :return: True or False
     '''
-    if 0 <= x < pow(2, 31) - 1:
-        y = 0.1
-        x_cache = x
-        while x != 0:
-            x //= 10
-            y *= 10
-        print(int(y))
-        print(x_cache)
-        while y >= 10:
-            print(x_cache % 10)
-            print(x_cache // y)
-            if x_cache % 10 == int(x_cache // y):
-                x_cache %= y
-                print(x_cache)
-                x_cache //= 10
-                print(x_cache)
-                y /= 100
-            else:
-                return False
-        return True
-    else:
+    if x < 0:
         return False
+    if x > 0 and x % 10 == 0:
+        return False
+    y = 0
+    x_cache = x
+    while x_cache:
+        y = y*10 + x_cache % 10
+        x_cache //= 10
+    return x == y
 
-num = 0
+num = 101
 print(ispalindrome(num))
+    #     y = 0.1
+    #     x_cache = x
+    #     while x != 0:
+    #         x //= 10
+    #         y *= 10
+    #     while y >= 10:
+    #         if x_cache % 10 == x_cache // y:
+    #             x_cache %= y
+    #             print(x_cache)
+    #             x_cache //= 10
+    #             print(x_cache)
+    #             y /= 100
+    #         else:
+    #             return False
+    #     return True
+    # else:
+    #     return False
+
