417901944be0a956970528d7550019f7b960cbee
konstter <konst.br@gmail.com>
2021-08-29 21:37:01 +0800

    7. Reverse Integer v2

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -4,23 +4,25 @@ def reverse(x):
     :param x: Integer
     :return:
     '''
-    if x != 0:
+    if x == 0:
+        return 0
+    else:
         int_max = pow(2, 31)-1
         int_min = pow(-2, 31)
         y = 0
-        while x != 0:
-            pop = abs(x) % 10
-            x //= 10
-            # if y > int_max/10 or (y == int_max/10 and pop > 7):
-            #     return 0
+        tmp = abs(x)
+        while tmp != 0:
+            pop = tmp % 10
+            tmp //= 10
+            if y > int_max/10 or (y == int_max/10 and pop > 7):
+                return 0
             # if y < int_min/10 or (y == int_min/10 and pop < -8):
             #     return 0
             y = y * 10 + pop
-        return y
-    else:
-        return 0
+        if x < 0:
+            return y * -1
+        else:
+            return y
 
-
-num = -7
-print(num % 5)
-# print(reverse(num))
+num = 71
+print(reverse(num))
