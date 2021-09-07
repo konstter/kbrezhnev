1a702e8802748f0a61128fd1edd7e42d093b829a
konstter <konst.br@gmail.com>
2021-09-07 10:43:53 +0800

    v2 t_26

--- 26.Remove_Duplicates_from_Sorted_Array.py
+++ 26.Remove_Duplicates_from_Sorted_Array.py
@@ -2,16 +2,17 @@
 #
 
 def removeduplicates(nums):
-    i = 0
-    l = len(nums)
-    while i <= l-2:
-        if nums[i] != nums[i+1]:
+    if not nums:
+        return 0
+    i, l = 0, len(nums)
+    for j in range(1, l):
+        if nums[j] != nums[i]:
             i += 1
-        else:
-            nums.remove(nums[i])
-            l -= 1
-    return l
+            nums[i] = nums[j]
+    print(nums)
+    return i + 1
 
 
-n = [1, 1, 2]
+n = [1, 1, 1, 2]
 print(removeduplicates(n))
+
