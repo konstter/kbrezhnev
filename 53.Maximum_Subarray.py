9dcb5385bd8f9f049b9534297cd71f94a0b18a8b
konstter <konst.br@gmail.com>
2021-09-08 09:37:38 +0800

    v3 t_53

--- 53.Maximum_Subarray.py
+++ 53.Maximum_Subarray.py
@@ -1,15 +1,16 @@
 # https://leetcode.com/problems/maximum-subarray/
 
 def maxsubarray(nums):
-    sum = 0
+    s = 0
     max = nums[0]
     for i in range(len(nums)):
-        sum += nums[i]
-        if sum > max:
-            max = sum
-        if sum <= 0:
-            sum = 0
+        s += nums[i]
+        if s > max:
+            max = s
+        if s <= 0:
+            s = 0
     return max
+
 
 n = [-5, -1, -3, -4]
 print(maxsubarray(n))
