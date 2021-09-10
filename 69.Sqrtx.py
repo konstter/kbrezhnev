88c80959baaf25665294339585dd78ae16470305
konstter <konst.br@gmail.com>
2021-09-10 08:42:10 +0800

    v1 69. Sqrt x

---
+++ 69.Sqrtx.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/sqrtx/
+
+def mysqrt(x):
+    y = 0
+    while y * y < x:
+        y += 10
+    while y * y > x:
+        y -= 1
+    if y * y <= x:
+        return y
+    else:
+        return y - 1
+
+given = 1024
+print(mysqrt(given))
+
