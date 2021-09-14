f5305cf8aed2dc77d91249fb38a49e3a16785f64
konstter <konst.br@gmail.com>
2021-09-14 19:57:11 +0800

    v1 88. Merge Sorted Array

---
+++ 88.Merge_Sorted_Array.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/merge-sorted-array/
+
+def merge(nums1, m, nums2, n):
+    i = m - 1
+    j = n - 1
+    l = m + n - 1
+
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[l] = nums1[i]
+            l -= 1
+            i -= 1
+        else:
+            nums1[l] = nums2[j]
+            l -= 1
+            j -= 1
+
+    while i >= 0:
+        nums1[l] = nums1[i]
+        l -= 1
+        i -= 1
+
+    while j >= 0:
+        nums1[l] = nums2[j]
+        l -= 1
+        j -= 1
+
+    return nums1
+
+print(merge([1,2,3,0,0,0], 3, [2,5,6], 3))
