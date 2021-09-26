9c8a64ebec1f13962725a0b420c7371cd24421e9
konstter <konst.br@gmail.com>
2021-09-26 13:17:39 +0800

    v1 104. Maximum Depth of B. Tree

---
+++ 108.Convert_Sorted_Array_to_BST.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def sortedarraytobst(nums):
+    if not nums: return None
+    c = len(nums) // 2
+    root = TreeNode(nums[c])
+    root.left = sortedarraytobst(nums[:c])
+    root.right = sortedarraytobst(nums[c+1:])
+    return root
+
+nums = [3, 5, 8]
+print(sortedarraytobst(nums).val)
