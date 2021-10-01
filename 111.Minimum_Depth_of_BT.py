e1a55fd978a382e5bf1accb21cd4e2b87e5ab8f3
konstter <konst.br@gmail.com>
2021-10-01 08:54:27 +0800

    v2 111. Minimum Depth of Binary Tree

--- 111.Minimum_Depth_of_BT.py
+++ 111.Minimum_Depth_of_BT.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/minimum-depth-of-binary-tree/submissions/
+# https://leetcode.com/problems/minimum-depth-of-binary-tree
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,23 +10,22 @@ class TreeNode:
         self.min = 999999
 
         def foo(h, k):
-
-            if h:
-                k += 1
-                if not h.left and not h.right:
-                    self.min = min(self.min, k)
-                return foo(h.left, k), foo(h.right, k)
+            if not h: return
+            k += 1
+            if not h.left and not h.right:
+                self.min = min(self.min, k)
+            return foo(h.left, k), foo(h.right, k)
 
         foo(root, 0)
         return self.min
 
 
-# root = TreeNode(1)
-# root.right = TreeNode(2)
-# root.right.right = TreeNode(3)
-# root.right.right.right = TreeNode(4)
-# root.right.right.right.right = TreeNode(5)
-# print(f'Result: {root.mindepth()}')
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.right = TreeNode(3)
+root.right.right.right = TreeNode(4)
+root.right.right.right.right = TreeNode(5)
+print(f'Result: {root.mindepth()}')
 
 
 # root = TreeNode(3)
@@ -40,13 +39,13 @@ class TreeNode:
 # root.left = TreeNode(2)
 # print(f'Result: {root.mindepth()}')
 
-root = TreeNode(-9)
-root.left = TreeNode(-3)
-root.left.right = TreeNode(4)
-root.left.right.left = TreeNode(-6)
-root.left.right.right = TreeNode(0)
-root.right = TreeNode(2)
-root.right.left = TreeNode(4)
-root.right.left.left = TreeNode(-5)
-root.right.right = TreeNode(0)
-print(f'Result: {root.mindepth()}')
+# root = TreeNode(-9)
+# root.left = TreeNode(-3)
+# root.left.right = TreeNode(4)
+# root.left.right.left = TreeNode(-6)
+# root.left.right.right = TreeNode(0)
+# root.right = TreeNode(2)
+# root.right.left = TreeNode(4)
+# root.right.left.left = TreeNode(-5)
+# root.right.right = TreeNode(0)
+# print(f'Result: {root.mindepth()}')
