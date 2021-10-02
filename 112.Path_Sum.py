ed8f0abd50b3a855ed0b5cc7a829da95bc4aa450
konstter <konst.br@gmail.com>
2021-10-02 11:47:33 +0800

    Revert "v1 112. Path Sum"
    This reverts commit 148499460bb3748d56ff1dda5ae37ba971066ce9.
    
    # Conflicts:
    #	112.Path_Sum.py

--- 112.Path_Sum.py
+++ 112.Path_Sum.py
@@ -6,18 +6,45 @@ class TreeNode:
         self.left = left
         self.right = right
 
-    def haspathsum(self, targetsum):
-        if not root: return False
-        self.f = False
-        def foo(r, pathsum):
-            if not r: return
-            pathsum += r.val
-            if not r.left and not r.right and pathsum == targetsum:
-                self.f = True
-            foo(r.left, pathsum)
-            foo(r.right, pathsum)
-        foo(root, 0)
-        return self.f
+def haspathsum(root, targetsum):
+    if not root: return False
+    s = []
+    pathsum, tmp = 0, 0
+    while True:
+        if not root and s:
+            item = s.pop()
+            root = item[0].right
+            tmp, pathsum = item[1], 0
+        else:
+            pathsum += root.val + tmp
+            tmp = 0
+            if not root.left and not root.right and pathsum == targetsum:
+                return True
+            s.append([root, pathsum])
+            root = root.left
+        if not root and not s:
+            return False
+
+
+
+# root = TreeNode(1)
+# root.right = TreeNode(2)
+# root.right.right = TreeNode(3)
+# root.right.right.right = TreeNode(4)
+# root.right.right.right.right = TreeNode(5)
+# print(f'Result: {haspathsum(root, 15)}')
+
+
+# root = TreeNode(-9)
+# root.left = TreeNode(-3)
+# root.left.right = TreeNode(4)
+# root.left.right.left = TreeNode(-6)
+# root.left.right.right = TreeNode(0)
+# root.right = TreeNode(2)
+# root.right.left = TreeNode(4)
+# root.right.left.left = TreeNode(-5)
+# root.right.right = TreeNode(0)
+# print(f'Result: {haspathsum(root, -8)}')
 
 root = TreeNode(1)
 root.left = TreeNode(-2)
@@ -26,4 +53,4 @@ root.left.left.left = TreeNode(-1)
 root.left.right = TreeNode(3)
 root.right = TreeNode(-3)
 root.right.left = TreeNode(-2)
-print(f'Result: {root.haspathsum(-4)}')
+print(f'Result: {haspathsum(root, -4)}')
