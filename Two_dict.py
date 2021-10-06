4b8419f68bc027e5f8ac6fe0fda6fbc68bdbf39d
konstter <konst.br@gmail.com>
2021-10-06 10:31:43 +0800

    v1 Two dict

--- Two_dict.py
+++ Two_dict.py
@@ -14,6 +14,7 @@ def two_dict(dict1, dict2):
 
 d1 = {'a': 'string', 'b': 5, 'c': [0, 1], 'd':{}}
 d2 = {'c': [0, 1], 'a': 'string', 'd':{}, 'b': 5}
+
 # d1 = d2 = {i: i + 1 for i in range(7)}
 print(two_dict(d1, d2))
 
