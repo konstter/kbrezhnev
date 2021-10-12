b804d8e3278000e55c4bb0a985084af17385c6e4
konstter <konst.br@gmail.com>
2021-10-12 23:19:29 +0800

    v2 Two dict

--- Two_dict.py
+++ Two_dict.py
@@ -1,19 +1,26 @@
 
 def two_dict(dict1, dict2):
-    l = list(dict1.keys())
+    def rec(dict1, dict2):
+        l = list(dict1.keys())
+        for item in l:
+            t1, t2 = type(dict1[item]), type(dict2[item])
+            if t1 != t2:
+                return False
+            else:
+                if t1 != dict:
+                    if t1 == list:
+                        dict1[item].sort()
+                        dict2[item].sort()
+                    if dict1[item] != dict2[item]:
+                        return False
+                else:
+                    return rec(dict1[item], dict2[item])
+    fl = rec(dict1, dict2)
+    return fl != False
 
-    def rec(l):
-        if not l:
-            return True
-        if dict1[l[-1]] != dict2[l[-1]]:
-            return False
-        l.pop()
-        return rec(l)
-    return rec(l)
 
-
-d1 = {'a': 'string', 'b': 5, 'c': [0, 1], 'd':{}}
-d2 = {'c': [0, 1], 'a': 'string', 'd':{}, 'b': 5}
+d1 = { 'a':1, 'b': { 'c': 1 } }
+d2 = { 'a':1, 'b': { 'c': 1 } }
 
 # d1 = d2 = {i: i + 1 for i in range(7)}
 print(two_dict(d1, d2))
