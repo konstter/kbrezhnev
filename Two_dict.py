d556eddfafeefd39739064d8799920a8ddca8092
konstter <konst.br@gmail.com>
2021-10-13 10:05:05 +0800

    v2 Two dict

--- Two_dict.py
+++ Two_dict.py
@@ -1,27 +1,25 @@
 
-def two_dict(dict1, dict2):
-    def rec(dict1, dict2):
-        l = list(dict1.keys())
-        for item in l:
-            t1, t2 = type(dict1[item]), type(dict2[item])
-            if t1 != t2:
-                return False
+
+def two_dict(dict1, dict2, path):
+    l = list(dict1.keys())
+    for item in l:
+        path += item
+        if isinstance(dict1[item], dict):
+            path += ' -> '
+            return two_dict(dict1[item], dict2[item], path)
+        else:
+            if isinstance(dict1[item], list):
+                dict1[item].sort()
+                dict2[item].sort()
+            if dict1[item] != dict2[item]:
+                print(path)
             else:
-                if t1 != dict:
-                    if t1 == list:
-                        dict1[item].sort()
-                        dict2[item].sort()
-                    if dict1[item] != dict2[item]:
-                        return False
-                else:
-                    return rec(dict1[item], dict2[item])
-    fl = rec(dict1, dict2)
-    return fl != False
+                path = ''
 
 
-d1 = { 'a':1, 'b': { 'c': 1 } }
-d2 = { 'a':1, 'b': { 'c': 1 } }
 
-# d1 = d2 = {i: i + 1 for i in range(7)}
-print(two_dict(d1, d2))
 
+d1 = { 'a':1, 'b': { 'c': { 'd': 1 } }, 'e': { 'f': { 'g': 2 } } }
+d2 = { 'a':1, 'b': { 'c': { 'd': 2 } }, 'e': { 'f': { 'g': 1 } } }
+
+two_dict(d1, d2, '')
