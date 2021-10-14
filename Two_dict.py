ffa528767c783410503ead21c280317aead1ead4
konstter <konst.br@gmail.com>
2021-10-14 09:15:26 +0800

    v4 Two dict

--- Two_dict.py
+++ Two_dict.py
@@ -1,22 +1,20 @@
 
 def two_dict(dict1, dict2, path):
     for key, value in dict1.items():
-        path += key
         if isinstance(value, dict):
-            path += ' -> '
-            return two_dict(dict1[key], dict2[key], path)
+            path += key + ' -> '
+            two_dict(dict1[key], dict2[key], path)
         else:
             if isinstance(value, list):
                 dict1[key].sort()
                 dict2[key].sort()
             if dict1[key] != dict2[key]:
+                path += key
                 print(path)
-            else:
-                path = ''
+        path = ''
 
 
-
-d1 = { 'a':1, 'b': { 'c': { 'd': 1 } }, 'e': { 'f': { 'g': 2 } } }
-d2 = { 'a':1, 'b': { 'c': { 'd': 2 } }, 'e': { 'f': { 'g': 1 } } }
+d1 = { 'b': { 'c': { 'd': 2 } }, 'e': { 'f': { 'g': 2 } }, 'h' : { 'i': [3, 2, 2] }, 'k': 1 }
+d2 = { 'b': { 'c': { 'd': 1 } }, 'e': { 'f': { 'g': 1 } } , 'h' : { 'i': [2, 1, 3] }, 'k': 1 }
 
 two_dict(d1, d2, '')
