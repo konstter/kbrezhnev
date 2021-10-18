6af6ae7a85a771e2dcf4e4c1c91d9141ea6793f2
konstter <konst.br@gmail.com>
2021-10-18 09:19:46 +0800

    v1 136. Single Number

---
+++ 136. Single Number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/single-number/
+
+def singlenumber(nums):
+    l = len(nums)
+    if l == 1: return nums[0]
+    nums.sort()
+    i = 0
+    while i < l - 1:
+        if nums[i] != nums[i+1]:
+            return nums[i]
+        i += 2
+    return nums[-1]
+
+
+if __name__ == '__main__':
+    print(singlenumber([3, 2, 5, 2, 5]))
