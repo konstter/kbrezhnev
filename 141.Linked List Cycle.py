88316b1657ba9e84da8eb9c529c17e72a16b5983
konstter <konst.br@gmail.com>
2021-10-18 10:13:48 +0800

    v1 141. Linked List Cycle

---
+++ 141.Linked List Cycle.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/linked-list-cycle/
+
+# https://leetcode.com/problems/linked-list-cycle/
+
+def hascycle(head):
+    if not head: return False
+    while True:
+        if not head.next:
+            return False
+        head.val = 'passed'
+        head = head.next
+        if head.val == 'passed':
+            return True
