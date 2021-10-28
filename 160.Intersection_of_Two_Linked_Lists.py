da472ab5fe6610cd1731f9b8dc06fdd6273ce212
konstter <konst.br@gmail.com>
2021-10-28 08:36:11 +0800

    v2 160. Intersection of Two Linked Lists

--- 160.Intersection_of_Two_Linked_Lists.py
+++ 160.Intersection_of_Two_Linked_Lists.py
@@ -1,19 +1,35 @@
 # https://leetcode.com/problems/intersection-of-two-linked-lists/
 
+def lengthoglist(head):
+    l = 0
+    while head:
+        l += 1
+        head = head.next
+    return l
+
+def newhead(head, delta):
+    while delta != 0:
+        head = head.next
+        delta -= 1
+    return head
+
 def getintersectionnode(headA , headB):
     if headA == headB: return headA
-    d = {}
+    hA, hB = headA, headB
+    a, b = lengthoglist(headA), lengthoglist(headB)
+    if a > b:
+        hA = newhead(headA, abs(a-b))
+    elif b > a:
+        hB = newhead(headB, abs(a-b))
 
-    while headA:
-        d[headA] = 1
-        headA = headA.next
-
-    while headB:
-        if headB in d:
-            return headB
-        headB = headB.next
+    while hA:
+        if hA == hB:
+            return hA
+        hA = hA.next
+        hB = hB.next
 
     return None
+
 
 
 
